0c156660872e662bbb1fa3e9764687598e6f81c8
Kevin Quispe <kevquispecc@gmail.com>
2026-05-01 16:48:21 -0500

    Liga1 pro - dto ESTADISTICAS jugadores

--- src/main/java/com/example/liga1pro/config/SecurityConfig.java
+++ src/main/java/com/example/liga1pro/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/partidos/**").permitAll()
                         .requestMatchers("/api/jugadores/**").permitAll()
                         .requestMatchers("/api/tabla/**").permitAll()
+                        .requestMatchers("/api/estadisticas/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
